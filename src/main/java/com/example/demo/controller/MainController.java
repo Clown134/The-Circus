9347288595b3be4ff62c386d42fb57cf9025dccd
гниша <gnishamilo>
2021-10-07 20:34:12 +0500

    z ifkjgfq

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,13 +1,16 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.*;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String CarouselTemplateBootstrapv5() {
+    public String CarouselTemplateBootstrapv5(Model model, @RequestParam(required = false) String string) {
+        model.addAttribute("ilia", string);
         return "index";
     }
+
 }
