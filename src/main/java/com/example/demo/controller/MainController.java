1819b5ad020434eee1a0481ecb158a0bb4e1b55c
dasem <gfhflbuvf46@gmail.com>
2021-10-28 19:27:57 +0500

    add buy mechanic

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -4,14 +4,50 @@ import org.springframework.stereotype.*;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import java.util.*;
 
 @Controller
 public class MainController {
 
+    public static Integer count = 0;
+
+    public List<Integer> countByProduct = new ArrayList<>();
+
+    {
+        countByProduct.add(0);
+        countByProduct.add(0);
+        countByProduct.add(0);
+    }
+
+    @GetMapping("/cart")
+    public String cart(Model model) {
+        model.addAttribute("countByProduct", countByProduct);
+        return "cart";
+    }
+
     @GetMapping("/")
-    public String CarouselTemplateBootstrapv5(Model model, @RequestParam(required = false) String string) {
+    public String index(Model model, @RequestParam(required = false) String string) {
         model.addAttribute("message", string);
+        return "index";
+    }
+
+    @GetMapping("/buy")
+    public String buy(Model model, @RequestParam(required = false) Integer id) {
+
+        Integer previousValue = countByProduct.get(id);
+        countByProduct.set(id, previousValue + 1);
+
+        model.addAttribute("message", id);
+        return "index";
+    }
+
+    @GetMapping("/sell")
+    public String sell(Model model, @RequestParam(required = false) Integer id) {
+
+        Integer previousValue = countByProduct.get(id);
+        countByProduct.set(id, previousValue - 1);
+
+        model.addAttribute("message", id);
         return "index";
     }
 }
