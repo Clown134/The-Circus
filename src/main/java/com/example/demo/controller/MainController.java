0aa25b84975548f3930a7a2f7370db4b4c2fbbcc
dasem <gfhflbuvf46@gmail.com>
2021-11-25 19:28:47 +0500

    some fixes

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controller;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.*;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,12 +11,12 @@ public class MainController {
 
     public static Integer count = 0;
 
-    public List<ProdAnima> products = new ArrayList<>();
+    public List<ProductAnimal> products = new ArrayList<>();
 
     {
-        products.add(new ProdAnima(0,"https://sun9-33.userapi.com/impg/Dvwv94QDPgV67mY5kcfzqjOC0LC4WPr_QVtlIA/Oye0B08AWfM.jpg?size=500x500&quality=96&sign=88236c2f0c7c6f81d9a790937b7d086a&type=album","Арбуз в сделку не входит","Кот на арбузе"));
-        products.add(new ProdAnima(0,"https://sun9-30.userapi.com/impg/5y7JYNiS10U4ZkQn0nqBs5UGc7uCAj5qONrvqQ/tRntNfvN4mw.jpg?size=500x500&quality=96&sign=39965f5eb54c88573ac163c3314ea5c0&type=album","Отдадим его в ваши руки очень неохотно","Потом узнаем"));
-        products.add(new ProdAnima(0,"https://sun9-49.userapi.com/impg/kCBm6GvhaBx_7LM84UNEupaAW5kyjrfEGTeBnQ/lzUCJuXla8M.jpg?size=500x500&quality=96&sign=cfdc136b2c3281c59c582a572bea4972&type=album","Большая и с очками","Обезьяна"));
+        products.add(new ProductAnimal(0,"https://sun9-33.userapi.com/impg/Dvwv94QDPgV67mY5kcfzqjOC0LC4WPr_QVtlIA/Oye0B08AWfM.jpg?size=500x500&quality=96&sign=88236c2f0c7c6f81d9a790937b7d086a&type=album","Арбуз в сделку не входит","Кот на арбузе"));
+        products.add(new ProductAnimal(0,"https://sun9-30.userapi.com/impg/5y7JYNiS10U4ZkQn0nqBs5UGc7uCAj5qONrvqQ/tRntNfvN4mw.jpg?size=500x500&quality=96&sign=39965f5eb54c88573ac163c3314ea5c0&type=album","Отдадим его в ваши руки очень неохотно","Потом узнаем"));
+        products.add(new ProductAnimal(0,"https://sun9-49.userapi.com/impg/kCBm6GvhaBx_7LM84UNEupaAW5kyjrfEGTeBnQ/lzUCJuXla8M.jpg?size=500x500&quality=96&sign=cfdc136b2c3281c59c582a572bea4972&type=album","Большая и с очками","Обезьяна"));
     }
 
     @GetMapping("/cart")
@@ -35,22 +33,22 @@ public class MainController {
 
     @PostMapping("/buy")
     public String buy(Model model, @RequestParam Integer id) {
-        ProdAnima prodAnima = products.get(id);
+        ProductAnimal prodAnima = products.get(id);
         Integer previousValue = prodAnima.getNumberPurchases();
         prodAnima.setNumberPurchases(previousValue + 1);
 
         model.addAttribute("products", products);
-        return "cart";
+        return "redirect:/cart";
     }
 
     @PostMapping("/sell")
     public String sell(Model model, @RequestParam Integer id) {
 
-        ProdAnima prodAnima = products.get(id);
+        ProductAnimal prodAnima = products.get(id);
         Integer previousValue = prodAnima.getNumberPurchases();
         prodAnima.setNumberPurchases(previousValue - 1);
 
         model.addAttribute("products", products);
-        return "cart";
+        return "redirect:/cart";
     }
 }
