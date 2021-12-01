dbb2ee3c7d571985baa788c04277e5ffdb402ca1
Beowulf4i <mr.karpenko.96@bk.ru>
2021-12-01 17:39:15 +0300

    Для Наташи

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -21,6 +21,12 @@ public class MainController {
         products.add(new ProdAnima(0,"https://sun9-49.userapi.com/impg/kCBm6GvhaBx_7LM84UNEupaAW5kyjrfEGTeBnQ/lzUCJuXla8M.jpg?size=500x500&quality=96&sign=cfdc136b2c3281c59c582a572bea4972&type=album","Большая и с очками","Обезьяна"));
     }
 
+    public List<ProdAnimaModal> CDG = new ArrayList<>();
+
+    {
+        CDG.add(new ProdAnimaModal("Полосатый кот-улыбака. С ним ваша жизнь станет ярче. Кот не оставит вас без внимания, в любое время вы сможете послушать от него забавные анекдоты.","https://sun9-33.userapi.com/impg/Dvwv94QDPgV67mY5kcfzqjOC0LC4WPr_QVtlIA/Oye0B08AWfM.jpg?size=500x500&quality=96&sign=88236c2f0c7c6f81d9a790937b7d086a&type=album","Любит рассказывать анекдоты.","Юморной котик"));
+    }
+
     @GetMapping("/cart")
     public String cart(Model model) {
         model.addAttribute("products", products);
@@ -30,6 +36,7 @@ public class MainController {
     @GetMapping("/")
     public String index(Model model, @RequestParam(required = false) String string) {
         model.addAttribute("products", products);
+        model.addAttribute("CDG", CDG);
         return "index";
     }
 
